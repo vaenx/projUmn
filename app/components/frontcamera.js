cfe02298e9f9737076822be62947539bf98b1a62
Vaenx <vanessaveigavilela@gmail.com>
2018-05-24 10:19:51 +0100

    Front camera test

--- app/components/frontcamera.js
+++ app/components/frontcamera.js
@@ -12,7 +12,7 @@ export default class FrontCamera extends React.Component {
             this.camera = ref;
           }}
           style = {styles.preview}
-          type={RNCamera.Constants.Type.back}
+          type={RNCamera.Constants.Type.front}
           flashMode={RNCamera.Constants.FlashMode.off}
           permissionDialogTitle={'Permission to use camera'}
           permissionDialogMessage={'We need your permission to use your camera'}>
